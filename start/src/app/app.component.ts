fe96ee8a3f85a36c6530283a7334a5004fa8db69
Titus Blair <titusblair@gmail.com>
2016-03-29 15:21:25 -1000

    Finalized code
    Finalized the code adding proper sound and assets to improve the
    gameplay.  Created a template start/ folder to work from and learn with
    code commented out.

---
+++ start/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import {Component} from 'angular2/angular2';
+import {MinesweeperComponent} from '../minesweeper/minesweeper.component';
+import {createGame} from '../minesweeper/game';
+
+@Component({
+  selector: 'app',
+  template: `
+  <minesweeper [game]="game" #minesweeper></minesweeper>
+  <ul class="actions">
+    <li><a (click)="startNewGame()">New game</a></li>
+    <li><a (click)="minesweeper.undo()" [hidden]="!minesweeper.canUndo()">Undo</a></li>
+   </ul>
+   <br/>
+  `,
+  directives: [MinesweeperComponent]
+})
+export class App {
+  public game;
+
+  constructor(){
+    
+  }
+  onInit(){
+    this.startNewGame();
+  }
+  startNewGame(){
+    this.game = createGame({cols: 16, rows: 16, mines: 48});
+  }
+
+} 
+/*
+
+    // add in the ul for muting sound
+    <li><a (click)="minesweeper.soundMixer.mute()">Sound: {{minesweeper.soundMixer.sound}}</a></li>
+
+    // add below game for credits
+    <div class="creditsbox">
+      <p [hidden]="!minesweeper.soundMixer.ddPlus"><a href="https://developer.dolby.com/" target="_new"><img class="dolbylogo" src="assets/DolbyLogo.png" /></a></p>
+      <p>Music: <a href="https://jukedeck.com/" target="_new">JukeDeck.com</a></p>
+      <p>Sound Effects: <a href="http://freesound.org/" target="_new">FreeSound.org</a></p>
+      <p>Code: <a href="http://www.jvandemo.com/how-to-build-minesweeper-using-angular-2-and-immutable-js/" target="_new">How to build Minesweeper using Angular 2 and Immutable.js</a></p>
+    </div>
+
+*/
+
